2f7d2bc0e94ba247f04b0c565060357e8180e8fd
Matthias Bolte <matthias@tinkerforge.com>
2021-01-19 16:05:25 +0100

    label: Don't assume that CWD is script home

--- label/print-esp32-label.py
+++ label/print-esp32-label.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3 -u
 
+import os
 import re
 import argparse
 import socket
@@ -17,15 +18,19 @@ SSID_PLACEHOLDER = b'esp32-XXXXXX'
 PASSPHRASE_PLACEHOLDER = b'ZZZZ-ZZZZ-ZZZZ-ZZZZ'
 
 def print_esp32_label(ssid, passphrase):
+    # check SSID
     if re.match('^(esp32|warp)-[{0}]{{3,6}}$'.format(BASE58), ssid) == None:
         raise Exception('Invalid SSID: {0}'.format(ssid))
 
+    # check passphrase
     if re.match('^[{0}]{{4}}-[{0}]{{4}}-[{0}]{{4}}-[{0}]{{4}}$'.format(BASE58), passphrase) == None:
         raise Exception('Invalid passphrase: {0}'.format(passphrase))
 
-    with open('esp32.prn', 'rb') as f:
+    # read EZPL file
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'esp32.prn'), 'rb') as f:
         data = f.read()
 
+    # patch QR code
     qr_code_command = QR_CODE_FORMAT.format(QR_CODE_LENGTH).encode('ascii')
 
     if data.find(qr_code_command) < 0:
@@ -33,16 +38,19 @@ def print_esp32_label(ssid, passphrase):
 
     data = data.replace(qr_code_command, QR_CODE_FORMAT.format(QR_CODE_LENGTH - len(SSID_PLACEHOLDER) + len(ssid)).encode('ascii'))
 
+    # patch SSID
     if data.find(SSID_PLACEHOLDER) < 0:
         raise Exception('SSID placeholder missing in EZPL file')
 
     data = data.replace(SSID_PLACEHOLDER, ssid.encode('ascii'))
 
+    # patch passphrase
     if data.find(PASSPHRASE_PLACEHOLDER) < 0:
         raise Exception('Passphrase placeholder missing in EZPL file')
 
     data = data.replace(PASSPHRASE_PLACEHOLDER, passphrase.encode('ascii'))
 
+    # print label
     with socket.create_connection((PRINTER_HOST, PRINTER_PORT)) as s:
         s.send(data)
 
