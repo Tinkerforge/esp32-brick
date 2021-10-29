8e0655a2d3a2e44dba2ea6a1782b6b0105d93eeb
Erik Fleckstein <erik@tinkerforge.com>
2021-10-29 10:12:41 +0200

    web_sockets: Handle payload more efficiently.

--- software/modules/backend/ws/ws.cpp
+++ software/modules/backend/ws/ws.cpp
@@ -53,10 +53,43 @@ void WS::addState(const StateRegistration &reg)
 
 }
 
+static const char *prefix = "{\"topic\":\"";
+static const char *infix = "\",\"payload\":";
+static const char *suffix = "}\n";
+static size_t prefix_len = strlen(prefix);
+static size_t infix_len = strlen(infix);
+static size_t suffix_len = strlen(suffix);
+
 void WS::pushStateUpdate(String payload, String path)
 {
-    String to_send = String("{\"topic\":\"") + path + String("\",\"payload\":") + payload + String("}\n");
-    web_sockets.sendToAll(to_send.c_str(), to_send.length());
+    if (!web_sockets.haveActiveClient())
+        return;
+    //String to_send = String("{\"topic\":\"") + path + String("\",\"payload\":") + payload + String("}\n");
+    size_t path_len = path.length();
+    size_t payload_len = payload.length();
+
+    size_t to_send_len = prefix_len + path_len + infix_len + payload_len + suffix_len;
+    char *to_send = (char*)malloc(to_send_len);
+    if (to_send == nullptr)
+        return;
+
+    char *ptr = to_send;
+    memcpy(ptr, prefix, prefix_len);
+    ptr += prefix_len;
+
+    memcpy(ptr, path.c_str(), path_len);
+    ptr += path_len;
+
+    memcpy(ptr, infix, infix_len);
+    ptr += infix_len;
+
+    memcpy(ptr, payload.c_str(), payload_len);
+    ptr += payload_len;
+
+    memcpy(ptr, suffix, suffix_len);
+    ptr += suffix_len;
+
+    web_sockets.sendToAllOwned(to_send, to_send_len);
 }
 
 void WS::wifiAvailable()
